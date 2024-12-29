3a60109557e6cc75743381fc1fceb191223dc8a2
Vrillault Gabin <gabin.vrillault@ecole.ensicaen.fr>
2024-12-29 01:57:08 +0100

    Action config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/undercover/',
+  base: process.env.VITE_PUBLIC_URL ? `/${process.env.VITE_PUBLIC_URL}/` : '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
+    minify: 'terser',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
